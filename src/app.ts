3ed2eaadb605e99fac9f4116220857e54745571b
Al Ameen S <alameensameen77@gmail.com>
2026-06-02 23:02:31 +0530

    feat : added metrics collection with promethues

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { errorHandler } from "./middlewares/app.middleware";
 import imageRouter from "./routes/image.routes";
+import { metricsMiddleware } from "./middlewares/metrics.middleware";
+import register from "./config/prom.client";
 
 dotenv.config();
 
@@ -12,11 +14,21 @@ const PORT = process.env.PORT || 5002;
 // app.use(cors());
 app.use(express.json());
 
+app.use(metricsMiddleware)
+
 app.get("/", (req, res) => {
   res.send("Media Service Running...");
 });
 
 app.use(process.env.API_IMAGE_ROUTE as string || "/api/v1/image", imageRouter);
+
+app.get(process.env.API_METRICS_ROUTE as string, async (_req, res) => {
+  res.set("Content-Type", register.contentType);
+
+  res.end(await register.metrics());
+
+});
+
 
 app.use(errorHandler)
 
